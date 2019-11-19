75c4ca02ab8278d51be9a80b01b9d07a30714b52
Owen Luke <hi.owen.luke@gmail.com>
2019-11-19 13:57:43 +0800

    Update receive.js

--- test/multi-queues/receive.js
+++ test/multi-queues/receive.js
@@ -3,14 +3,18 @@ const rabbitMQ = require('../../src');
 const mq = rabbitMQ();
 
 const handler = async (message) => {
-	console.log(message);
+	console.log('[1]', message);
+};
+
+const handler2 = async (message) => {
+	console.log('[2]', message);
 };
 
 const main = async () => {
 
 	// Tell rabbitmq to call handler when there is a new message
 	await mq.receive('qx1', handler);
-	await mq.receive('qx2', handler);
+	await mq.receive('qx2', handler2);
 };
 
 main();
