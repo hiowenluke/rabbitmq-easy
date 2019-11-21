ae5f3e05ba3b67fe9ba59a528cbfebbe8f472194
Owen Luke <hi.owen.luke@gmail.com>
2019-11-21 17:58:30 +0800

    move code to __lib.js

--- src/mq.js
+++ src/mq.js
@@ -2,7 +2,7 @@
 const connect = require('./connect');
 const lib = require('./__lib');
 
-const main = {
+const me = {
 	host: 'localhost',
 	queue: 'default',
 
@@ -69,33 +69,8 @@ const main = {
 	}
 };
 
-const create = (queue, host, options) => {
-
-	// create()
-	if (!queue) {
-		// do nothing
-	}
-	else
-
-	// create({queue, host, options})
-	if (typeof queue === 'object') {
-		({queue, host, options} = queue);
-	}
-	else {
-		// create(queue, options)
-		if (typeof host === 'object') {
-			options = host;
-			host = null;
-		}
-		else {
-			// create(queue, host, ...)
-			// do nothing
-		}
-	}
-
-	const inst = Object.create(main);
-	inst.init(queue, host, options);
-	return inst;
+const create = (...args) => {
+	return lib.create(me, ...args);
 };
 
 module.exports = create;
