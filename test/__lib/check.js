be5e5a7fa6e17e83efb425d39a9e1847426a6b02
Owen Luke <hi.owen.luke@gmail.com>
2019-11-23 00:12:14 +0800

    Update check.js

--- test/__lib/check.js
+++ test/__lib/check.js
@@ -1,34 +1,32 @@
 
 const me = {
-    startTime: null,
-    lastCount: null,
-    handler: null,
-    counter: null,
+	startTime: null,
+	handler: null,
+	counter: null,
+	maxTimes: 0,
 
-    start(counter) {
-        this.counter = counter;
-        this.startTime = new Date().getTime();
+	start(counter, maxTimes) {
+		this.counter = counter;
+		this.maxTimes = maxTimes;
+		this.startTime = new Date().getTime();
 
-        // There will be problems below 100 milliseconds
-        this.handler = setInterval(() => {this.verify()}, 100);
-    },
+		// There will be problems below 100 milliseconds
+		this.handler = setInterval(() => {this.verify()}, 100);
+	},
 
-    verify() {
-        const count = this.counter();
+	verify() {
+		const count = this.counter();
+		if (count < this.maxTimes) {
+			return;
+		}
 
-        if (this.lastCount !== count) {
-            this.lastCount = count;
-            return;
-        }
+		clearInterval(this.handler);
 
-        clearInterval(this.handler);
-
-        const endTime = new Date().getTime();
-        const during = (endTime - this.startTime) / 1000;
-        const avg = Math.floor(count / during);
-
-        console.log(`Done. \nTotal: ${count}, During: ${during}s, Avg: ${avg}/s`);
-    }
+		const endTime = new Date().getTime();
+		const during = (endTime - this.startTime) / 1000;
+		const avg = Math.floor(count / during);
+		console.log(`Done. \nTotal: ${count}, During: ${during}s, Avg: ${avg}/s`);
+	}
 };
 
 module.exports = me;
