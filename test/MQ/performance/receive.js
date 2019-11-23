1a0e8a77fb510595794aa3be140fb2f6371a0501
Owen Luke <hi.owen.luke@gmail.com>
2019-11-24 00:42:12 +0800

    Update receive.js

--- test/MQ/performance/receive.js
+++ test/MQ/performance/receive.js
@@ -1,24 +1,15 @@
 
-const check = require('../../__lib/check');
 const MQ = require('../../../src').MQ;
 
 const queue = 'q2';
 const mq = MQ(queue);
 
-let count = 0;
-
 const handler = async (message) => {
 	// console.log(message);
-	count ++;
 };
 
 const main = async () => {
 	await mq.receive(handler);
 };
 
-(async () => {
-	const counter = () => {return count};
-	check.start(counter);
-
-	await main();
-})();
+main();
