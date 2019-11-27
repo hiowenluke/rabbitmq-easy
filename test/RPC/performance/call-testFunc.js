c790cb2fe0e924884e0fdee7d170825f3b3cdef3
Owen Luke <hi.owen.luke@gmail.com>
2019-11-27 09:48:12 +0800

    Update call-testFunc.js

--- test/RPC/performance/call-testFunc.js
+++ test/RPC/performance/call-testFunc.js
@@ -3,10 +3,12 @@ const be = require('benchmark-easy')();
 const RPC = require('../../../src').RPC;
 const rpc = RPC();
 
+// 1000 times, 2.826 seconds, 353/s
+const times = 1000;
+
 const main = async (count) => {
 	const result = await rpc.call('testFunc', count);
-	// console.log(count, result);
+	times <= 100 && console.log(count, result);
 };
 
-// about 20 seconds, avg 300 ops/sec
-be.start(main, 10000);
+be.start(main, times);
